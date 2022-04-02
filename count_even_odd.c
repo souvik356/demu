7bb52de83f8cecdb05fffca47f6549a928a59580
souvik356 <72182566+souvik356@users.noreply.github.com>
2022-04-03 01:23:38 +0530

    c program to find the count of odd or even

---
+++ count_even_odd.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int  main(){
+  int n,i,even=0,odd=0;
+  scanf("%d",&n);
+  int arr[n];
+  for(i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+  }
+  for(i=0;i<n;i++){
+    if(arr[i] % 2==0){
+       even=even+1;
+    }
+    else{
+        odd=odd+1;
+    }
+  }
+  printf("Even elements in the array are= %d",even);
+  printf("\nOdd elements in the array are= %d",odd);
+  return 0;
+}
